95f55c7b6af5628e859e32da7c2772a4700e3dc8
David Schmitz <david.schmitz.privat@gmail.com>
2017-09-19 15:35:18 +0200

    Working on test

--- index.js
+++ index.js
@@ -3,32 +3,35 @@ const Wreck = require('wreck');
 const jsonfile = require('jsonfile');
 
 const extractEventTitles = function($) {
-    const events = {};
-    $('div.event').filter((i, el) => {
-        const $element = $(el);
-        const eventId = $element.attr('event_id');
-        const [date, title] = $element.find('div.date').text().trim().split(' / ');
+    return new Promise( (resolve, reject) => {
+        const events = {};
+        $('div.event').filter((i, el) => {
+            const $element = $(el);
+            const eventId = $element.attr('event_id');
+            const [date, title] = $element.find('div.date').text().trim().split(' / ');
 
-        console.log(`Store event ${eventId} with title ${title}`);
-        events[eventId] = {
-            date,
-            title
-        };
+            events[eventId] = {
+                date,
+                title
+            };
+        });
+        resolve(events);
     });
-    return events;
 };
 
 const extractSummary = function($) {
-    const events = {};
-    $('div.content').filter((i, el) => {
-        const $element = $(el);
-        const eventId = $element.attr('event_id');
-        const summary = $element.find('div.text').text().trim();
+    return new Promise( (resolve, reject) => {
+        const events = {};
+        $('div.content').filter((i, el) => {
+            const $element = $(el);
+            const eventId = $element.attr('event_id');
+            const summary = $element.find('div.text').text().trim();
 
-        const event = events[eventId];
-        events[eventId] = {summary};
+            const event = events[eventId];
+            events[eventId] = {summary};
+        });
+        resolve(events);
     });
-    return events;
 };
 
 const mergeEvents = function (titles, summaries) {
@@ -36,9 +39,10 @@ const mergeEvents = function (titles, summaries) {
     for (let entryId in titles) {
         mergedEvents[entryId] = Object.assign({}, titles[entryId], summaries[entryId]);
     }
+    return mergedEvents;
 };
 
-const fetchEvents = function(callback) {
+const fetchPage = function(callback) {
     Wreck.get('https://senacor.com/news-events/', {
         timeout: 2000,
         maxBytes: 100000
@@ -46,21 +50,46 @@ const fetchEvents = function(callback) {
         if (error || response.statusCode !== 200) {
             callback(error);
         }
-        const $ = cheerio.load(body);
+        callback(null, body);
+    });
+};
 
-        const titles = extractEventTitles($);
-        const summaries = extractSummary($);
+const extractEvents = function (page) {
+    const $ = cheerio.load(page);
 
-        callback(null, mergeEvents(titles, summaries));
-    }
-    );
+    return Promise.all([ extractEventTitles($), extractSummary($) ])
+         .then( results => {
+            const [titles, summaries] = results;
+            return mergeEvents(titles, summaries);
+         })
+         .catch( rejection => { throw rejection; } );
+};
+
+const handler = function (event, context, callback) {
+    fetchPage( (err, body) => {
+        if (err) {
+            callback(err);
+        }
+        else {
+            extractEvents(body)
+                .then(events => callback(null, events))
+                .catch(error => callback(error));
+        }
+    });
 };
 
 module.exports = {
-    'handler': function (event, context, callback) {
-        fetchEvents(callback);
-    },
+    'handler': handler,
     'extractEventTitles': extractEventTitles,
     'extractSummary': extractSummary,
     'mergeEvents': mergeEvents
 };
+
+if (process.env.NODE_ENV === 'development') {
+    handler(null, null, (err, events) => {
+        if (err) {
+            console.warn(err);
+        }
+        console.log(events);
+    } );
+}
--- test/test.js
+++ test/test.js
@@ -1,4 +1,37 @@
 const testdata = require('./test_data').homePage;
 const Main = require('../index');
 
-console.log(testdata);
+const expect = require('chai').expect;
+const chai = require('chai');
+const sinon = require('sinon');
+const fs = require('fs');
+
+describe('Event fetch lambda', function () {
+    it('should extract the titles from the homepage', (done) => {
+        done();
+    });
+
+    it('should reject if extracting titles fails', (done) => {
+        done();
+    });
+
+    it('should extract the summaries from the homepage', (done) => {
+        done();
+    });
+
+    it('should reject if extracting summaries fails', (done) => {
+        done();
+    });
+
+    it('should merge titles and summaries', (done) => {
+        done();
+    });
+
+    it('should fetch the page', (done) => {
+        done();
+    });
+
+    it('should report fetch errors', (done) => {
+        done();
+    });
+});
